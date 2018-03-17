462dad2d3ff14b5011869daa3f31daad8cecc984
Matan <matanrak@me.com>
2018-03-17 16:05:55 +0200

    Added multiprocessing & split parsing to sentences in order to increase speed

--- rake.py
+++ rake.py
@@ -2,29 +2,48 @@
 # Created in 2010 by S. Rose, D. Engel, N. Cramer and W. Cowley
 # https://pdfs.semanticscholar.org/5a58/00deb6461b3d022c8465e5286908de9f8d4e.pdf
 
-import re
+from multiprocessing import Pool
+import re, os
+
+
+def unwrapped_candidates_from_sentence(arg, **kwarg):
+    return Rake.get_candidates_from_sentence(*arg, **kwarg)
 
 
 class Rake:
 
     def __init__(self, text, nlp):
-        self.text = text
+
         self.nlp = nlp
-        self.parsed = nlp.parse(text)
-        self.sentences = self.parsed['sentences']
+
+        # Removes all non-unicode characters and quotation marks from the text for cleaner sentence splitting
+        self.text = text.replace('"', '')
+        self.text = re.sub(r'[^\x00-\x7F]+', '', self.text)
+
+        # Although corenlp has sublime sentence splitting, In order to save time I decided to pre split the sentences
+        self.sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', self.text)
 
     def get_key_words(self):
-        try:
-            candidates = self.get_candidates_from_text()
-            return self.calculate_candidate_scores(candidates)
-        except UnicodeError:
-            return []
 
-    def get_candidates_from_text(self):
+        pool = Pool(processes=8)
+        results = pool.map_async(unwrapped_candidates_from_sentence, zip([self]*len(self.sentences), self.sentences))
+
+        pool.close()
+        pool.join()
+
+        results = results.get()
+
+        candidates = [candidate for sub_array in results for candidate in sub_array]
+
+        return self.calculate_candidate_scores(candidates)
+
+    def get_candidates_from_sentence(self, sentence_string):
 
         candidates = []
 
-        for sentence in self.sentences:
+        try:
+            sentence = self.nlp.parse(annotators='depparse pos', data=sentence_string)['sentences'][0]
+
             for index in xrange(0, len(sentence['tokens'])):
 
                 # Verifying that a word is a noun
@@ -32,8 +51,9 @@ class Rake:
                     continue
 
                 phrase = {sentence['tokens'][index]['word']: index}
+
                 phrase.update(
-                    {entity['dependentGloss']: entity['dependent']
+                    {entity['dependentGloss']: entity['dependent'] - 1
                      for entity in sentence['basicDependencies']
                      if entity['governor'] - 1 is index
                      and entity["dep"] in ['compound', 'amod']
@@ -44,6 +64,9 @@ class Rake:
                 phrase = ' '.join([part[0] for part in phrase]).strip()
 
                 candidates.append(phrase)
+
+        except (IndexError, UnicodeError) as exception:
+            print 'Failed to parse: ', sentence, os.linesep, exception
 
         return candidates
 
